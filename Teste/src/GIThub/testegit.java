fcf85485ca7cba3beba5a79547669f762d948c36
Fernandosouza33 <71730038+Fernandosouza33@users.noreply.github.com>
2020-09-30 17:13:56 -0300

    Test Java inicio

---
+++ Teste/src/GIThub/testegit.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package GIThub;
+
+/**
+ * @author fernando.souza
+ *
+ */
+public class testegit {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello github");
+		
+
+	}
+
+}
---
+++ Teste/src/app.java
@@ -0,0 +1,54 @@
+import java.util.Timer;
+
+public class app {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Programação Realizada");
+		System.out.println("Teste de Variaveis");
+		int a = 10;
+		char b = 'i';
+		long c = 400;
+		double d = 4.5;
+		////////////////////
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+		System.out.println(d);
+		
+		//////////////////////
+		
+		System.out.println(a+c);
+		System.out.println(a>=b/2);
+	
+		int temp = 800;
+		System.out.println(temp-1);
+		temp= 30-1;
+		System.out.println(temp);
+		//////////////////////////
+		//y=x+2=0//
+		int y = 4;
+		int x = 5;
+		float resultado = 0;
+		resultado = y+x;
+		
+		System.out.println(resultado);
+		
+
+	
+
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		 
+	
+
+	}
+
+}
